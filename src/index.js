acbd615c129cae2469429ac6e7f9462620503188
Paolo Rossin <112543287+githubdeaththrow03@users.noreply.github.com>
2023-06-24 11:23:11 +0800

    Merge branch 'master' of https://github.com/githubdeaththrow03/KPJ-PROJECT into Pao

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from"react-router-dom";
+import Home from "./Views/Home/Home";
+import Registration from "./Views/Registration/Registration";
+import About from "./Views/About/About";
+import Gallery from "./Views/Gallery/Gallery";
+
+const router = createBrowserRouter ([
+  { path: '/', element: <Home /> },
+  { path: 'about', element: <About /> },
+  { path: 'registration', element: <Registration /> },
+  { path: 'gallery', element: <Gallery /> },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
