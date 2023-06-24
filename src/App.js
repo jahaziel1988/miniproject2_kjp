ea3086ddb60c35b91bcb5ee2d7db7dd2bc5efada
Paolo Rossin <112543287+githubdeaththrow03@users.noreply.github.com>
2023-06-24 11:17:27 +0800

    Edited some css style in Navbar.css aand Registration.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Home from "./Views/Home/Home.js";
 import Navbar from "./Views/Navbar/Navbar";
+import Registration from "./Views/Registration/Registration.js";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Registration/>
     </div>
   );
 }
