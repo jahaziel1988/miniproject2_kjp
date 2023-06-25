a5852d62d46b330e7f6ca55cb51aebfb6dbefa27
Paolo Rossin <112543287+githubdeaththrow03@users.noreply.github.com>
2023-06-25 21:23:41 +0800

    Changed google font link in index html. Changed css properties and class attribute in About page.

--- src/Views/About/About.js
+++ src/Views/About/About.js
@@ -9,31 +9,36 @@ const About = () => {
     return (
         <div>
             <Navbar />
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src={slide_1_picture} class="d-block w-100 carousel-img img-fluid carousel-img-1" alt=""></img>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={slide_1_picture} className="d-block w-100 carousel-img carousel-img-1" alt=""></img>
                     </div>
-                    <div class="carousel-item">
-                        <img src={slide_2_picture} class="d-block w-100 carousel-img img-fluid" alt=""></img>
+                    <div className="carousel-item">
+                        <img src={slide_2_picture} className="d-block w-100 carousel-img" alt=""></img>
                     </div>
-                    <div class="carousel-item">
-                        <img src={slide_3_picture} class="d-block w-100 carousel-img img-fluid" alt=""></img>
+                    <div className="carousel-item">
+                        <img src={slide_3_picture} className="d-block w-100 carousel-img" alt=""></img>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
                 </button>
+            </div>
+            <div className='about-section container-fluid p-3'>
+                <p className='p-5 m-0'>
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                </p>
             </div>
         </div>
     );
