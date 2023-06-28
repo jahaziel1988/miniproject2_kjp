4371d20f73187b301cb0d798bff76339f12808b5
Paolo Rossin <112543287+githubdeaththrow03@users.noreply.github.com>
2023-06-28 21:25:16 +0800

    Added anchor element

--- src/Views/Navbar/Navbar.js
+++ src/Views/Navbar/Navbar.js
@@ -11,15 +11,15 @@ const Navbar = () => {
   return (
     <nav className="navbar" style={{ backgroundColor: "#B755F2" }}>
       <div className="navbar-logo">
-        <img src={logo} alt="2KLC Logo" className='logo' style={{ width: "100px", height: "auto", margin: "10px" }} />
-        <h2 className="display-5 m-0">Lebrown City</h2>
+        <a href="/" className="navbar-lebrown-city-link"><img src={logo} alt="2KLC Logo" className='m-0' style={{ width: "100px", height: "auto", margin: "10px" }} /></a>
+        <a href="/" className="navbar-lebrown-city-link d-none d-sm-block"><h2 className="display-5 m-0">Lebrown City</h2></a>
       </div>
       <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'bi bi-x' : 'bi bi-list'} />
       </div>
       <div className={click ? 'nav-menu active' : 'nav-menu'}>
-        <a href="about" className="navbar-link h5" onClick={closeMobileMenu}>About Us</a>
-        <a href="registration" className="navbar-link h5" onClick={closeMobileMenu}>Registration Form</a>
+        <a href="about" className="navbar-link h5" onClick={closeMobileMenu}>Registration Form</a>
+        <a href="registration" className="navbar-link h5" onClick={closeMobileMenu}>About Us</a>
         <a href="gallery" className="navbar-link h5" onClick={closeMobileMenu}>Gallery</a>
       </div>
     </nav>
